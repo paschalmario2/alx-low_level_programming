63d015ac03a5ce8b431d254a50da6817ae7ca5c4
paschalmario2 <zikorapascal@gmail.com>
2022-12-10 10:45:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * return: Always 0
  * */
 int main (void)	{
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
