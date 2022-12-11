8b4fe700807d9c4780b57bc2ded8451ce9fec00c
paschalmario2 <zikorapascal@gmail.com>
2022-12-11 07:52:00 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ *Return: Always 1 (Success)
+ **/
+int main (void) {
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (1);
+}
