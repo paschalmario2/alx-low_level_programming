5e94c7cd63cebbb6138dc4a92cf47bdf1884e3cc
paschalmario2 <zikorapascal@gmail.com>
2022-12-16 19:35:06 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if paremeter is an uppercase letter
+ * @c: input character
+ * Return: 1 if an uppercase letter, 0 if it is other case
+ */
+int _isupper(int c)
+	if (c >= 65 && c <= 90)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
