906894c36eaf7920ee8c97108fcb3000d6ffd5bf
paschalmario2 <zikorapascal@gmail.com>
2022-12-20 08:44:22 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - This funtion prints reverse string
+ *
+ * @s: This is the input string
+ */
+void print_rev(char *s)
+{
+int index;
+for (index = 0; s[index] != '\0' ; index++)
+for (index = index - 1; s[index] != '\0'; index++)
+{
+	_putchar(s[index]);
+}
+_putchar('\n');
+}
