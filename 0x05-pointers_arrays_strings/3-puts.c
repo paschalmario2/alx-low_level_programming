297f3edb779c3ddebcf4d47565b536d180b93fac
paschalmario2 <zikorapascal@gmail.com>
2022-12-19 19:13:54 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _puts - write function that prints a string
+ * followed by a new line to stdout.
+ */
+void _puts(char *str);
+{
+int index;
+for (index = 0; str[index] !=
