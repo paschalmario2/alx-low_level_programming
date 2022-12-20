875ece53590d5f7f7b6c5d51f9d92cd0476b42a1
paschalmario2 <zikorapascal@gmail.com>
2022-12-19 18:28:46 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - this is to counr the number
+ * of bytes in a string.
+ * @s - this is the input string
+ * Return: length of the string
+ */
+int _strlen(char *s);
+{
+int index;
+for (index = 0; s[index] |= '\0'; index++)
+return (index);
+}
