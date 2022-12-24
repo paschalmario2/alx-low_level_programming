915372d375cf8fd9060f76073bc227ec89a81a6c
paschalmario2 <zikorapascal@gmail.com>
2022-12-23 17:42:46 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alays 0
+ */
+int main(void)
+{
+	char d;
+	char c;
+
+	d = 'a';
+	c = 'A';
+	while
+		(d <= 'z')
+{
+	putchar(d);
+	d++;
+}
+while
+(c <= 'Z')
+{
+	putchar(c);
+	c++;
+}
+return (0);
+}
