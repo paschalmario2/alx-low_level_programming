f4f52114b0037286f04bb92372fe597d54d7dd1d
paschalmario2 <zikorapascal@gmail.com>
2023-01-04 10:35:49 -0800

    something

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\n')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
 
-	else
-		_putchar('\n');
+		_putchar (*s);
+		_puts_recursion(s + 1);
 }
 
