c5431905590e973b03d760629e454ef82d13b3e6
paschalmario2 <zikorapascal@gmail.com>
2023-01-04 16:38:40 -0800

    0-puts_recursion hope i finnally got it

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
-	}
-
-		_putchar (*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
+	}
+	
+	else
+		_putchar('\n');
 }
 
