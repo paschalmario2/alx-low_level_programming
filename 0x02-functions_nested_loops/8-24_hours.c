3a41b6b18c4570ec7fb55f2ca6257d8ef934d495
paschalmario2 <zikorapascal@gmail.com>
2023-01-06 08:51:06 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0, a <= 2, a++)
+	for (a = 0; a <= 2; a++)
 	{
-	for (b = 0, b <= 4, b++)
+	for (b = 0; b <= 4; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 4))
 	{
-	for (c = 0, c <= 5, c++)
+	for (c = 0; c <= 5; c++)
 	{
-	for (d = 0, c <= 9, c++)
+	for (d = 0; c <= 9; c++)
 	{
 	_putchar(a + '0');
 	_putchar(b + '0');
