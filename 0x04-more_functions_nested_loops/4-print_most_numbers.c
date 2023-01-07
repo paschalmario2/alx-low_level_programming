988c265d1e385b05a33bf15336f617a35949dde4
paschalmario2 <zikorapascal@gmail.com>
2023-01-07 08:07:41 -0800

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most number
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+	}
+}
