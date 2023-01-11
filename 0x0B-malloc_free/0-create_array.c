fd123d75bfbc2393655b0fd9fb5b6a3e3f033d40
paschalmario2 <zikorapascal@gmail.com>
2023-01-10 16:33:30 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * create_array - array for prints a string
  * @size: numbers of elements
@@ -15,6 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
+	/*define values with malloc*/
 	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0)
@@ -24,11 +28,12 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 		position = 0;
-		while (position < size)
+		while (position < size)/* while for array */
 		{
-			*(buffer + position) + c;
+			*(buffer + position) = c;
 			position++;
 		}
 
 		return (buffer);
 	}
+}
