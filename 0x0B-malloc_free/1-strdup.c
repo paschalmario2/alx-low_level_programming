47ce3876cd8722747287ec811e86a354862029a7
paschalmario2 <okaforzikora020gmail.com@>
2023-10-03 17:16:56 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,41 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - Concatenates two strings using at
- * most an inputted number of bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1.
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the concatenated space in memory.
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_strdup(char *str)
 {
-	char *concat;
-	unsigned int len = n, index;
+	char *strout;
+	unsigned int i, j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-
-	for (index = 0; s1[index]; index++)
-		len++;
-	concat = malloc(sizeof(char) * (len + 1));
-
-
-	if (concat == NULL)
+	if (str == NULL)
 		return (NULL);
-	len = 0;
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
 
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-	concat[len] = '\0';
-	return (concat);
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
 }
-
